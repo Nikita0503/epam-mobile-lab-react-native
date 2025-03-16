e951b62091fa3db84606e2fd2922e3e1ba328559
Shevtsiv Nikita <nikita0503ua@gmail.com>
2025-03-16 17:59:02 +0200

    feat: Login Screen

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,11 +1,30 @@
+import useAuth from '@hooks/useAuth';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, TextInput, View } from 'react-native';
 import styles from './styles';
 
 const LoginScreen = () => {
+  const [email, setEmail] = React.useState<string>('');
+  const [password, setPassword] = React.useState<string>('');
+
+  const {signIn} = useAuth();
+
+  const onLoginPress = React.useCallback(() => {
+    signIn(email, password);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [email, password]);
+
   return (
     <View style={styles.container}>
-      <Text>LoginScreen</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput value={email} onChangeText={setEmail} placeholder="Email" />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput value={password} onChangeText={setPassword} placeholder="Password" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Login" onPress={onLoginPress}/>
+      </View>
     </View>
   );
 };
--- src/screens/LoginScreen/styles.ts
+++ src/screens/LoginScreen/styles.ts
@@ -3,6 +3,14 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 12,
+  },
+  textInputContainer: {
+    marginBottom: 12,
+  },
+  buttonContainer: {
+    width: '50%',
+    alignSelf: 'center',
   },
 });
 
