c6beba30647363ee5e17fceeb548e5e5d4540fb5
Shevtsiv Nikita <nikita0503ua@gmail.com>
2025-03-23 18:39:26 +0200

    feat: refresh token

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -9,6 +9,8 @@ import axios, {
   AxiosResponse,
   InternalAxiosRequestConfig,
 } from 'axios';
+import dayjs from 'dayjs';
+import { jwtDecode } from 'jwt-decode';
 import { refreshApi } from './authApi';
 
 const axiosInstance = axios.create({
@@ -18,6 +20,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig<any>) => {
     const accessToken = store.getState().auth.accessToken;
+    console.log(config.url, accessToken);
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
@@ -33,25 +36,32 @@ axiosInstance.interceptors.response.use(
   async (error: AxiosError) => {
     const originalRequest = error.config!;
     if (error.response?.status === 401) {
-      const refreshToken = store.getState().auth.refreshToken;
-      try {
-        if (!refreshToken) {
-          return Promise.reject(error);
+      const accessToken = store.getState().auth.accessToken;
+      const decoded: any = jwtDecode(accessToken!);
+      const expirationDate = dayjs(decoded.exp);
+      const currentDate = dayjs();
+      if (currentDate.diff(expirationDate) > 0) {
+        const refreshToken = store.getState().auth.refreshToken;
+        try {
+          if (!refreshToken) {
+            return Promise.reject(error);
+          }
+          const res = await refreshApi(refreshToken);
+          const newAccessToken = res.tokens.accessToken;
+          const newRefreshToken = res.tokens.refreshToken;
+          store.dispatch(setAccessTokenAction({ accessToken: newAccessToken }));
+          store.dispatch(
+            setRefreshTokenAction({ refreshToken: newRefreshToken }),
+          );
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return axiosInstance(originalRequest);
+        } catch (refreshError) {
+          store.dispatch(setAccessTokenAction({ accessToken: undefined }));
+          store.dispatch(setRefreshTokenAction({ refreshToken: undefined }));
+          return Promise.reject(refreshError);
         }
-        const res = await refreshApi(refreshToken);
-        const newAccessToken = res.tokens.accessToken;
-        const newRefreshToken = res.tokens.refreshToken;
-        store.dispatch(setAccessTokenAction({ accessToken: newAccessToken }));
-        store.dispatch(
-          setRefreshTokenAction({ refreshToken: newRefreshToken }),
-        );
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        return axiosInstance(originalRequest);
-      } catch (refreshError) {
-        store.dispatch(setAccessTokenAction({ accessToken: undefined }));
-        store.dispatch(setRefreshTokenAction({ refreshToken: undefined }));
-        return Promise.reject(refreshError);
       }
+      return Promise.reject(error);
     }
     return Promise.reject(error);
   },
