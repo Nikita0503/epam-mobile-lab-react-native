9bb7004f141ebc07dc79f100f327e9cf74695c02
Shevtsiv Nikita <nikita0503ua@gmail.com>
2025-03-25 00:36:06 +0200

    feat: styles

--- styles.ts
+++ styles.ts
@@ -1,6 +1,11 @@
 import { Colors } from '@theme/colors';
 import { Platform, StatusBar, StyleSheet } from 'react-native';
 
+let statusBarHeight = StatusBar.currentHeight;
+if (statusBarHeight === undefined || statusBarHeight === 0) {
+  statusBarHeight = 50;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -9,7 +14,7 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: Colors.violet,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === 'android' ? statusBarHeight : 0,
   },
 });
 
