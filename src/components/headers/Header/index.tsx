de0f01a49a0b93c2e9fc179fa691e00bd7bf684e
Shevtsiv Nikita <nikita0503ua@gmail.com>
2025-03-25 23:18:25 +0200

    feat: small improvements

--- src/components/headers/Header/index.tsx
+++ src/components/headers/Header/index.tsx
@@ -21,11 +21,15 @@ const Header = ({ title, actionButton, hideBackButton }: IProps) => {
   const navigation = useNavigation<NavigationProp<AppStackParamList>>();
 
   const onPressBackButton = React.useCallback(() => {
-    // needs to handle universal links proccess
-    navigation.reset({
-      index: 0,
-      routes: [{ name: ERouteNames.TABS_SCREEN }],
-    });
+    if (navigation.canGoBack()) {
+      navigation.goBack();
+    } else {
+      // need to handle universal links proccess
+      navigation.reset({
+        index: 0,
+        routes: [{ name: ERouteNames.TABS_SCREEN }],
+      });
+    }
   }, [navigation]);
 
   return (
